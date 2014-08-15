4f6f8ff5831781f9f7947e1d43766764d50eeca1
Matt Kwiecien <mkwieci2@gmail.com>
2014-08-15 13:23:07 -0500

    re-structured the indexing of the correction so it is accurate now.  but auto-finding ROI is still limited to one object file.

--- image_profile.py
+++ image_profile.py
@@ -44,51 +44,60 @@ def datafit(N_FILES):
 		xi,xf = d[0][0], d[0][-1]
 
 		new_ob = np.angle(ob[yi+20:yf-20, xi+20:xf-20])
-
 		x_len, y_len = shape(new_ob)
+
 		x_dim = new_ob[x_len-1,:]
-		x_diff = np.diff(x_dim[:800])
-		x_loc = np.where(abs(x_diff)>1.5)
-		x_start = x_loc[0][0]
-		x_end = x_loc[0][-1] + 40
 
-		fit_start = new_ob[:, :x_start]
-		fit_end = new_ob[:, x_end:]
-		fit = np.hstack((fit_start,fit_end))
+		x_loc = np.where(x_dim[200:800]>1.5)
 
-		r = sp.zeros((2,x_len*y_len))
+		f1_start = x_loc[0][0] + 200
+		f2_start = x_loc[0][-1] + 240
+
+
+		x = sp.zeros((x_len*y_len))
+		y = sp.zeros((x_len*y_len))
 		z = sp.zeros((x_len*y_len))
 		cnt=0
-		for i in range(y_len):
-			for j in range(x_len):
+		for i in range(x_len):
+			for j in range(y_len):
 				if j>f1_start and j<f2_start:
 					pass
 				else:
-					r[cnt] = (i,j)
-					z[cnt] = new_ob[r[i,j]]
+					x[cnt] = i
+					y[cnt] = j
+					z[cnt] = new_ob[i,j]
 
 				cnt+=1
 
-		#SKETCH
-		x_points = fit[1,:]
-		y_points = fit[:,1]
-		m,n = shape(fit)
-		xp=np.linspace(0,m,m, endpoint=False)
-		yp=np.linspace(0,n,n, endpoint=False)
+		fitfunc = lambda p, x, y: p[0] + p[1]*y + p[2]*x + p[3]*x*y \
+		+ p[4]*(y**2) + p[5]*(y**2)*x + p[6]*(x**2) + p[7]*(x**2)*y \
+		+ p[8]*(x**2)*(y**2)
 
-		xg,yg = meshgrid(x_points, y_points)
-
-		fitfunc = lambda p, r: p[0] + p[1]*r[1] + p[2]*r[0] + p[3]*r[0]*r[1]\
-		+ p[4]*(r[1]**2) + p[5]*(r[1]**2)*r[0] + p[6]*(r[0]**2) + p[7]*(r[0]**2)*r[1] \
-		+ p[8]*(r[0]**2)*(r[1]**2)
-		errfunc = lambda p,r,z: abs(fitfunc(p,r)-z)
+		errfunc = lambda p,x,y,z: abs(fitfunc(p,x,y)-z)
 
 		p0 = [1, 1, 1, 1, 1, 1, 1, 1, 1]
-		p1, success = optimize.leastsq(errfunc, p0[:], args=(r,z))
 
-		xt = new_ob[1,:]
-		yt = new_ob[:,1]
+		p1, success = optimize.leastsq(errfunc, p0[:], args=(x,y,z))
 
-		xgt,ygt = meshgrid(xt,yt)
-		corr = fitfunc(p1,xgt,ygt)
+		xn = sp.zeros((x_len*y_len))
+		yn = sp.zeros((x_len*y_len))
+		cnt2=0
+		for i in range(x_len):
+			for j in range(y_len):
+				xn[cnt2] = i
+				yn[cnt2] = j
+
+			cnt2 +=1
+		
+		count=0
+		corr_ob = sp.zeros(shape(new_ob))
+		corr = fitfunc(p1,xn,yn)
+		for i in range(x_len):
+			for j in range(y_len):
+				corr_ob[i,j]=corr[count]
+			count+=1
+
+
+		corr_img = np.divide(new_ob, corr_ob)
+		corr_img = np.divide(corr_img, np.average(corr_img))
 		new_img = np.subtract(new_ob, corr)
